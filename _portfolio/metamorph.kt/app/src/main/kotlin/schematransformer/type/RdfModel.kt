0a470b44fb96d1c23ffb9e354dd70bede9d5bad7
Bart Kleijngeld <bartkl@gmail.com>
2025-08-31 17:02:08 +0200

    Replaced old resume with new one

--- _portfolio/metamorph.kt/app/src/main/kotlin/schematransformer/type/RdfModel.kt
+++
@@ -1,6 +0,0 @@
-package schematransformer.type
-
-import org.eclipse.rdf4j.model.Model
-import java.io.File
-
-data class RdfModel(val path: File, val data: Model)
--- _portfolio/metamorph.kt/app/src/main/kotlin/schematransformer/util/Rdf.kt
+++
@@ -1,8 +0,0 @@
-package schematransformer.util
-
-import org.eclipse.rdf4j.model.IRI
-import org.eclipse.rdf4j.model.util.Values
-import java.io.File
-
-fun getFileIRI(base: File, relativeURL: String): IRI =
-    Values.iri(File(base, relativeURL).normalize().toURI().toString())
